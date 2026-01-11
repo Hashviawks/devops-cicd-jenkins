7910c32863faa4fbda054383458af13384bd8780
Hashvitha A <hashvitha@elmeasure.com>
2026-01-12 03:25:16 +0530

    Test check

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def Hello():
-    return "Hello I have been successfully built, tested and devployed by Jenkins and Docker. YAYY!!"
+    return "Hello I have been successfully built, tested and devployed by Jenkins and Docker. YAY!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
